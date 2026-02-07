4de80095a197eaf07ffcc07d2276bed73ec9e58d
Sushmitha112 <sushgowda83@gmail.com>
2026-02-07 13:43:46 +0530

    Add Python fundamentals script with functions and imports

---
+++ src/Day-5/day-5_python_fundamental.py
@@ -0,0 +1,30 @@
+def greet():
+    print("hello,welcome to internship")
+greet()    
+def add(a,b):
+  return a+b
+result=add(5,3)
+print("the sum is:",result)  
+
+x=10
+def show_value():
+    x=5
+    print(x)
+show_value()
+print(x)    
+
+icecream="venilla"
+def change_flavor():
+    
+    icecream="chocolate"
+    print("inside function:",icecream)
+change_flavor()
+print("outside function:",icecream)     
+
+
+import math
+import random
+print(math.sqrt(4))
+print(random.uniform(10,20))
+
+
